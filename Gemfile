ff8dede4d250ae61026dc1b150f64647809d8f24
Michael Bianchi <michaeldbianchi@gmail.com>
2019-05-07 19:13:43 -0500

    Format

--- Gemfile
+++ Gemfile
@@ -2,7 +2,10 @@
 source "https://rubygems.org"
 
 gem 'semantic_logger'
+# gem 'activesupport'
+# gem 'opentracing'
 gem 'faraday'
-gem 'faraday_middleware'
-gem 'typhoeus'
+# gem 'faraday-tracer'
+# gem 'faraday_middleware'
+# gem 'typhoeus'
 # gem 'concurrent-ruby', require: 'concurrent'
--- main.rb
+++ main.rb
@@ -1,63 +1,66 @@
-#TODO implement service
-#TODO implement logging
+# TODO: implement service
+# TODO implement logging
 
 require 'date'
 require 'yaml'
 
+# require 'opentracing'
+# OpenTracing.global_tracer = TracerImplementation.new
+
 require 'faraday'
-require 'faraday_middleware'
-require 'typhoeus/adapters/faraday'
+# require 'faraday/tracer'
+# require 'faraday_middleware'
+# require 'typhoeus/adapters/faraday'
 
 require 'semantic_logger'
 
-# TODO move to networked DB
+# TODO: move to networked DB
 options = (YAML.load_file 'checks.yaml')
 
-checkOptions = options["checks"]
-defaultOptions = options["checkDefaults"]
-
+checkOptions = options['checks']
+defaultOptions = options['checkDefaults']
 
 # Set defaults
-DEFAULT_CHECK_FREQUENCY = defaultOptions["frequency"] || 10
-DEFAULT_CHECK_PROTOCOL = defaultOptions["protocol"] || "https"
-DEFAULT_CHECK_ENDPOINT = defaultOptions["endpoint"] || "/testall"
+DEFAULT_CHECK_FREQUENCY = defaultOptions['frequency'] || 10
+DEFAULT_CHECK_PROTOCOL = defaultOptions['protocol'] || 'https'
+DEFAULT_CHECK_ENDPOINT = defaultOptions['endpoint'] || '/testall'
 
-def init_logger logger_name
+def init_logger(logger_name)
   SemanticLogger.default_level = :trace
   SemanticLogger.add_appender(file_name: 'development.log', level: :info, formatter: :color)
   SemanticLogger.add_appender(io: $stdout, level: :debug, formatter: :color)
 
   logger = SemanticLogger[logger_name]
-  
-    # Logging.color_scheme( 'bright',
-    #     :levels => {
-    #         :info  => :green,
-    #         :warn  => :yellow,
-    #         :error => :red,
-    #         :fatal => [:white, :on_red]
-    #     },
-    #     :date => :blue,
-    #     :logger => :cyan,
-    #     :message => :magenta
-    # )
-    # Logging.appenders.stdout(
-    #   'stdout',
-    #   :level  => :debug,
-    #   :layout => Logging.layouts.pattern(
-    #     :color_scheme => 'bright'
-    #   )
-    # )
 
-    # Logging.appenders.rolling_file(
-    #   'example.log',
-    #   :level  => :info,
-    #   :layout => Logging.layouts.json
-    # )
+  # Logging.color_scheme( 'bright',
+  #     :levels => {
+  #         :info  => :green,
+  #         :warn  => :yellow,
+  #         :error => :red,
+  #         :fatal => [:white, :on_red]
+  #     },
+  #     :date => :blue,
+  #     :logger => :cyan,
+  #     :message => :magenta
+  # )
+  # Logging.appenders.stdout(
+  #   'stdout',
+  #   :level  => :debug,
+  #   :layout => Logging.layouts.pattern(
+  #     :color_scheme => 'bright'
+  #   )
+  # )
 
-    # logger = Logging.logger[logger_name]
+  # Logging.appenders.rolling_file(
+  #   'example.log',
+  #   :level  => :info,
+  #   :layout => Logging.layouts.json
+  # )
 
-    # logger.add_appenders 'stdout', 'example.log'
-    return logger
+  # logger = Logging.logger[logger_name]
+
+  # logger.add_appenders 'stdout', 'example.log'
+  logger
 end
 
 $logger = init_logger 'simple_uptime_logger'
@@ -66,99 +69,99 @@ class InitializationInvalidError < StandardError; end
 
 class Check
   include SemanticLogger::Loggable
-    attr_accessor :next_time, :last_time, :resps
-    attr_reader :frequency, :name, :uri
-    
-    def initialize options={}
-        if !options["name"] || !options["host"]
-            $logger.warn "Required information missing. Ignoring check: #{options}"
-            @valid = false
-            return
-        end
+  attr_accessor :next_time, :last_time, :resps
+  attr_reader :frequency, :name, :uri
 
-        @name = options["name"]
-        @uri = Faraday::Utils::URI("#{options["protocol"] || DEFAULT_CHECK_PROTOCOL}://#{options["host"]}#{options["endpoint"] || DEFAULT_CHECK_ENDPOINT}")
-        @next_time = Time.now.utc.to_f
-        @frequency = options["frequency"]
-        @resps = Hash.new()
+  def initialize(options = {})
+    if !options['name'] || !options['host']
+      $logger.warn "Required information missing. Ignoring check: #{options}"
+      @valid = false
+      return
     end
 
-    def valid?
-        return @valid != false
-    end
+    @name = options['name']
+    @uri = Faraday::Utils::URI("#{options['protocol'] || DEFAULT_CHECK_PROTOCOL}://#{options['host']}#{options['endpoint'] || DEFAULT_CHECK_ENDPOINT}")
+    @next_time = Time.now.utc.to_f
+    @frequency = options['frequency']
+    @resps = {}
+  end
 
-    def eval conn
-        conn.get @uri
-    end
+  def valid?
+    @valid != false
+  end
 
-    def add_resp t, resp
-        @resps[t] = resp
-    end
+  def eval(conn)
+    conn.get @uri
+  end
 
-    def remove_resp t
-        @resps.delete(t)
-    end
+  def add_resp(t, resp)
+    @resps[t] = resp
+  end
 
-    private
-    #TODO implement
-    def gen_uri host #endpoint="/" protocol="https"
-        Faraday::Utils::URI("#{options["protocol"]}://#{options["host"]}#{options["endpoint"]}")
-    end
+  def remove_resp(t)
+    @resps.delete(t)
+  end
+
+  private
+
+  # TODO: implement
+  def gen_uri(_host) # endpoint="/" protocol="https"
+    Faraday::Utils::URI("#{options['protocol']}://#{options['host']}#{options['endpoint']}")
+  end
 end
 
 checks = []
-for checkOption in checkOptions
-    check = Check.new checkOption
-    checks << check if check.valid?
+checkOptions.each do |checkOption|
+  check = Check.new checkOption
+  checks << check if check.valid?
 end
 
-def eval_checks checks
-    t = Time.now.utc.to_f
-    evaluatedChecks = []
-    conn = Faraday::Connection.new() do |c|
-        # c.use :instrumentation
-        c.adapter :typhoeus
-    end
+def eval_check(check)
+  t = Time.now.utc.to_f
 
-    conn.in_parallel do
-        for check in checks
-            if check.next_time < t
-                # set check time
-                check.last_time = t
-                check.next_time = check.next_time + (check.frequency || DEFAULT_CHECK_FREQUENCY)
+  conn = Faraday.new do |c|
+    # c.use Faraday::Tracer, span: span
+    c.adapter Faraday.default_adapter
+  end
+  # p "evaling #{check.name}"
 
-                check.add_resp(t, check.eval(conn))
-
-                evaluatedChecks << check
-            end
-        end
-    end
-    log_checks t, evaluatedChecks
+  # set check time
+  if check.next_time < t
+    check.last_time = t
+    check.next_time = check.next_time + (check.frequency || DEFAULT_CHECK_FREQUENCY)
+    start_time = Time.now.utc.to_f
+    check.add_resp(t, check.eval(conn))
+    end_time = Time.now.utc.to_f
+    duration = end_time - start_time
+    log_check t, check, duration
+  end
 end
 
-def log_checks t, checks
-    checks.each do |check|
-        $logger.info "#{Time.at(check.last_time).utc} - #{check.name} - #{check.resps[t].status} - #{check.resps[t].body} - #{Time.at(check.next_time).utc}"
-        # p check.resps[t]
-        check.remove_resp(t)
-    end
+def log_check(t, check, duration)
+  $logger.info "#{Time.at(check.last_time).utc} - #{check.name} - #{check.resps[t].status} - #{check.resps[t].body} - #{Time.at(check.next_time).utc} - #{duration.round(3)}s"
+  check.remove_resp(t)
 end
 
-$logger.info "Check Time - Check Name - Check Status - Check Body - Next Check Due Time"
+$logger.info 'Check Time - Check Name - Check Status - Check Body - Next Check Due Time - Duration'
 
-threads = Hash.new()
-while true
-    threads[Time.now.utc.to_f] = Thread.new { eval_checks checks }
-
-    # delete old threads
-
-    sleep 0.1
-    threads.each do |t, thread|
-        if t < Time.now.utc.to_f - 2
-            thread.kill
-        end
+threads = {}
+loop do
+  checks.each do |check|
+    # p "#{check.name} - #{Time.at(check.next_time)}"
+    if check.next_time < Time.now.utc.to_f
+      # p check
+      threads[Time.now.utc.to_f] = Thread.new { eval_check check }
     end
-    threads = threads.select do |t, thread|
-        thread.alive?
-    end
+    sleep 0.01
+  end
+
+  sleep 0.1
+  # delete old threads
+
+  threads.each do |t, thread|
+    thread.kill if t < Time.now.utc.to_f - 2
+  end
+  threads = threads.select do |_t, thread|
+    thread.alive?
+  end
 end
