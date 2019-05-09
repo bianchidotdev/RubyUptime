ddf9473aa5387d63587377460f5c1a9539bdb0f3
Michael Bianchi <michaeldbianchi@gmail.com>
2019-05-09 07:40:45 -0500

    Refactor

--- Gemfile
+++ Gemfile
@@ -2,10 +2,4 @@
 source 'https://rubygems.org'
 
 gem 'semantic_logger'
-# gem 'activesupport'
-# gem 'opentracing'
 gem 'faraday'
-# gem 'faraday-tracer'
-# gem 'faraday_middleware'
-# gem 'typhoeus'
-# gem 'concurrent-ruby', require: 'concurrent'
--- main.rb
+++ main.rb
@@ -38,15 +38,22 @@ class InitializationInvalidError < StandardError; end
 
 class Check
   include SemanticLogger::Loggable
-  attr_accessor :next_time, :last_time, :resps
-  attr_reader :frequency, :name, :uri
+  attr_accessor :last_time
+  attr_reader :next_time, :name, :uri, :reqs
+
+  def self.log_header
+    logger.info(
+      "Check Time - Check Name - Check Status - Check Body - Next Check Due Time - \
+Duration"
+  )
+  end
 
   def initialize(options = {})
     self.name = options['name']
     @uri = gen_uri options
     @next_time = Time.now.utc.to_f
     @frequency = options['frequency'] || DEFAULT_CHECK_FREQUENCY
-    @resps = {}
+    @reqs = {}
   rescue ArgumentError => e
     logger.warn "Error creating check. Ignoring: #{e}"
     @valid = false
@@ -77,12 +84,35 @@ class Check
     [resp, duration]
   end
 
-  def add_resp(time, resp)
-    @resps[time] = resp
+  def set_next_time
+    @next_time = @next_time + @frequency
   end
 
-  def remove_resp(time)
-    @resps.delete(time)
+  def add_request(time, request)
+    @reqs[time] = request
+  end
+
+  def remove_request(time)
+    @reqs.delete(time)
+  end
+
+  def successful?
+    return true
+  end
+
+  def on_success(time)
+    # p self.reqs[time][:resp]
+    logger.info(
+      "#{Time.at(self.last_time).utc} - \
+#{self.name} - \
+#{self.reqs[time][:resp].status} - \
+#{self.reqs[time][:resp].body} - \
+#{Time.at(self.next_time).utc} - \
+#{self.reqs[time][:duration].round(3)}s"
+    )
+  end
+
+  def on_failure(time)
   end
 
   private
@@ -110,59 +140,47 @@ def eval_check(check, logger)
   time = Time.now.utc.to_f
 
   conn = Faraday.new do |c|
-    # c.use Faraday::Tracer, span: span
     c.adapter Faraday.default_adapter
   end
-  # p "evaling #{check.name}"
 
-  # set check time
   if check.next_time < time
     check.last_time = time
-    check.next_time = check.next_time + check.frequency
+    check.set_next_time
     resp, duration = check.eval(conn)
-    check.add_resp(time, resp)
-    # eval_resp resp
-    log_check time, check, duration, logger
+    request = {
+      :resp => resp,
+      :duration => duration,
+    }
+    check.add_request(time, request)
+
+    check.successful? ? check.on_success(time) : check.on_failure(time)
+    check.remove_request(time)
   end
 end
 
-def log_check(time, check, duration, logger)
-  logger.info(
-    "#{Time.at(check.last_time).utc} - \
-#{check.name} - \
-#{check.resps[time].status} - \
-#{check.resps[time].body} - \
-#{Time.at(check.next_time).utc} - \
-#{duration.round(3)}s"
-  )
-  check.remove_resp(time)
-end
-
-logger.info(
-  'Check Time - Check Name - Check Status - Check Body - Next Check Due Time - \
-  Duration'
-)
-
 checks = create_checks(check_options)
+
+Check.log_header
 
 threads = {}
 loop do
   checks.each do |check|
-    # p "#{check.name} - #{Time.at(check.next_time)}"
     if check.next_time < Time.now.utc.to_f
-      # p check
       threads[Time.now.utc.to_f] = Thread.new { eval_check check, logger }
     end
     sleep 0.001
   end
 
   sleep 0.1
-  # delete old threads
 
+  # delete old threads
   threads.each do |time, thread|
-    thread.kill if time < Time.now.utc.to_f - 2
+    thread.kill if (time < Time.now.utc.to_f - 2)
   end
+
   threads = threads.select do |_time, thread|
     thread.alive?
   end
+
+  logger.warn("Spawning too many threads - #{threads}") if threads.count > (checks.count * 3)
 end
