e7aad9ca7502097c6f2aba556463e8c8b631f652
Michael Bianchi <michaeldbianchi@gmail.com>
2019-07-12 09:34:07 -0500

    Load user config

--- lib/ruby_uptime/logger.rb
+++ lib/ruby_uptime/logger.rb
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-class RubyUptime::Logger
-  def initialize log_file="#{APP_ENV}.log"
-    SemanticLogger.default_level = :trace
-    SemanticLogger.add_appender(
-      file_name: "#{AppConfig.paths.log_dir}/#{log_file}", level: :info, formatter: :json
-    )
-    SemanticLogger.add_appender(
-      io: $stdout, level: :debug, formatter: :color
-    )
-
-    logger = SemanticLogger[logger_name]
-
-    logger
-  end
-end
-
-def logger(logger_name)
-  logger ||=begin
-    SemanticLogger.default_level = :trace
-    SemanticLogger.add_appender(
-      file_name: '../development.log', level: :info, formatter: :json
-    )
-    SemanticLogger.add_appender(
-      io: $stdout, level: :debug, formatter: :color
-    )
-
-    logger = SemanticLogger[logger_name]
-
-    logger
-=======
 module RubyUptime
   module Logger
     def self.init_logger(logger_name, log_file="#{APP_ENV}.log")
@@ -43,7 +12,6 @@ module RubyUptime
   
       logger
     end
->>>>>>> c5eedbc04b6958363c5a58ba036cb595d18f5b7a
   end
 end
 
@@ -53,10 +21,5 @@ end
 # caller_locations.first.lineno                 # line number of caller
 
 def logger
-<<<<<<< HEAD
-  logger||=RubyUptime::Logger.new
-=======
-  binding.pry
-  logger||=RubyUptime::Logger.init_logger("RubyUptime")
->>>>>>> c5eedbc04b6958363c5a58ba036cb595d18f5b7a
+  @logger||=RubyUptime::Logger.init_logger("RubyUptime")
 end
--- lib/ruby_uptime/user_config.rb
+++ lib/ruby_uptime/user_config.rb
@@ -1,12 +1,51 @@
 class RubyUptime::UserConfig
+  attr_reader :check_config
+
   def initialize
-    files
+    user_defaults = load_default_config
+    checks = check_config
+    # need to merge with default key || default
+    binding.pry
   end
 
   private
 
   def files
     dir = AppConfig.paths.check_config_dir
-    logger.debug(Dir["#{dir}/**/*.yml*"])
+    files = Dir["#{dir}/**/*.yml*"]
+    files.delete(default_config_path)
+    logger.debug("Found check config files #{files.join(", ")}")
+    files
+  end
+
+  def default_config_path
+    "#{AppConfig.paths.check_config_dir}/#{AppConfig.paths.check_defaults_file}"
+  end
+
+  def load_default_config
+    load_file(default_config_path) if File.file?(default_config_path)
+  end
+
+  def check_config
+    @check_config ||=begin
+      checks_array = files.map { |file| load_file(file) }.reject{ |c| !c }
+      checks_keys = checks_array.map { |e| e.keys }.flatten
+      dups = checks_keys.group_by{ |e| e }.keep_if{ |_, e| e.length > 1 }
+      logger.warn("Duplicate checks exist! There may be strange merging behavior: #{dups.keys.join(', ')}") unless dups.empty?
+      checks_array.reduce(:merge)
+    end
+  end
+
+  def load_file(filename)
+    contents = File.read(filename)
+    case
+    when filename.match?(/\.yml$/)
+      YAML.load(contents)
+    when filename.match?(/\.yml.erb$/)
+      erb = ERB.new(contents).result
+      YAML.load(erb)
+    when filename.match?(/\.json$/)
+      JSON.parse(contents)
+    end
   end
 end
